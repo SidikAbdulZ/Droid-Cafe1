8cd676bd9b8e11e0377a8518e2dcfac60e152394
Sidik_RajinNgoding <sidikjabar115@gmail.com>
2025-10-21 11:36:32 +0700

    Droid Cafe ver.2

--- app/src/main/java/lat/pam/droidcafe/OrderActivity.kt
+++ app/src/main/java/lat/pam/droidcafe/OrderActivity.kt
@@ -1,20 +1,54 @@
 package lat.pam.droidcafe
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.AdapterView
+import android.widget.RadioButton
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class OrderActivity : AppCompatActivity() {
+class OrderActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_order)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val citySpinner = findViewById<Spinner>(R.id.city_spinner)
+        citySpinner.onItemSelectedListener = this
+    }
+
+    fun onRadioButtonClicked(view: View) {
+        // Is the button now checked?
+        val checked = (view as RadioButton).isChecked
+        when (view.getId()) {
+            R.id.sameday -> if (checked) // Same day service
+                displayToast(getString(R.string.same_day_messenger_service))
+
+            R.id.nextday -> if (checked) // Next day delivery
+                displayToast(getString(R.string.next_day_ground_delivery))
+
+            R.id.pickup -> if (checked) // Pick up
+                displayToast(getString(R.string.pick_up))
+
+            else -> {}
         }
+    }
+
+    private fun displayToast(message: String) {
+        Toast.makeText(
+            applicationContext, message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        val item = parent.getItemAtPosition(pos).toString()
+        if (pos != 0) {
+            displayToast(item)
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        // Another interface callback
     }
 }
